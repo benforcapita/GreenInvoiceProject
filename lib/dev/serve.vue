05cbc1f7064ad4c899cffe92407fcdb91264fc79
benforcapita <benforcapita@gmail.com>
2021-04-12 15:05:21 +0300

    initial

---
+++ lib/dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { LibSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  LibSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <lib-sample />
+  </div>
+</template>
